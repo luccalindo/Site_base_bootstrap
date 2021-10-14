6ebabce7f9fe321e8395995b7ea5ee22f34bdf4d
luccafsx <78920755+luccalindo@users.noreply.github.com>
2021-10-14 10:24:54 -0300

    trocaazul

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,8 @@ function trocaverde(){
     const texto = document.getElementById("quem")
     texto.style.color = "green";
 }
+
+function trocaazul(){
+    const texto = document.getElementById("artistas")
+    texto.style.color = "blue";
+}
